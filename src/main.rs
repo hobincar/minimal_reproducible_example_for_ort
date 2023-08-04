f92fd19871094571636913a6161b4aef82859747
ks.kim <ks.kim@naverlabs.com>
2023-08-04 15:03:25 +0900

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::path::Path;
+use std::sync::Arc;
+use std::vec::Vec;
+
+use ndarray::{CowArray, Dim, IxDynImpl, arr1};
+use ort::{
+    tensor::OrtOwnedTensor,
+    Environment, ExecutionProvider, GraphOptimizationLevel, OrtResult, SessionBuilder, Value
+};
+
+
+fn func() -> OrtResult<OrtOwnedTensor<'static, f32, Dim<IxDynImpl>>> {
+    let environment = Arc::new(
+        Environment::builder()
+            .with_execution_providers([ExecutionProvider::CPU(Default::default())])
+            .build()?
+    );
+    let session = SessionBuilder::new(&environment)?
+        .with_optimization_level(GraphOptimizationLevel::Disable)?
+        .with_model_from_file(Path::new(&String::from("softmax.onnx")))?;
+
+    let input = CowArray::from(arr1(&[1f32, 2f32, 3f32, 4f32])).into_dyn();
+
+    let output: Vec<Value> = session.run(vec![
+        Value::from_array(session.allocator(), &input)?,
+    ])?;
+    let output: OrtOwnedTensor<f32, _> = output[0].try_extract()?;
+    println!("[1] output: {:?}", output);
+
+    Ok(output)
+}
+
+
+fn main() {
+    let output = func();
+
+    println!("[2] output: {:?}", output.unwrap());
+}
+
